ea439aeb603c2e1daf2167bc9d67be54d1cb9cbc
megha081298 <83633539+megha081298@users.noreply.github.com>
2022-05-14 13:28:04 +0530

    Add files via upload

---
+++ Multiple linear regressor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 14 12:49:27 2022
+
+@author: apple
+"""
+
+import pandas as pd
+dataset=pd.read_csv("Largecap_Balancesheet.csv")
+
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,5].values
+
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+ct= ColumnTransformer([("Country",OneHotEncoder(),[4])],remainder="passthrough")
+X=ct.fit_transform(X)
+
+X=X[:,1:]
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+
+from sklearn.linear_model import LinearRegression
+lr=LinearRegression()
+lr.fit(X_train,y_train)
+
+y_pred=lr.predict(X_test)
+print(lr.score(X_test,y_test))
